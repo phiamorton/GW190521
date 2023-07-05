c548436ce805dfa95fee53322317f448dd84f42c
phiamorton <mortonso@oregonstate.edu>
2023-07-05 12:00:43 +0200

    estimate of pdf

--- radius_prior.py
+++ radius_prior.py
@@ -7,16 +7,19 @@ from scipy.stats import laplace
 def prob(logslope, intercept, width1, width2, peakheight1, peakheight2, center1, center2, logr):
     linear= logslope * logr +intercept
     #normalize it in log space
-    area= logslope*logr.max()**2+intercept*logr.max() - (logslope*logr.min()**2+intercept*logr.min())
-    linear/=area
-    #c=2 the scipy documentation is a lie
-    peak1= laplace.logpdf(logr, center1, width1/10)
-    peak2=laplace.logpdf(logr, center2, width2/10)
-    return linear #+ peak1 +peak2)/3 # /3 because each is normalized 
+    #area= logslope*logr.max()**2+intercept*logr.max() - (logslope*logr.min()**2+intercept*logr.min())
+    #linear/=area
 
+    #c=2 the scipy documentation is a lie
+    peak1= (laplace.pdf(logr, center1, width1/100))/10
+    peak2=(laplace.pdf(logr, center2, width2/100))/10
+    return linear + peak1 +peak2      # /3 if each is normalized IF WANT TO NORMALIZE? MAYBE?
+
+#linear in log scale:
 logslope= 1
 intercept=1
 
+#Laplacian/resonance peaks:
 center1=0.85
 center2=1.61
 width1=np.log(2)
@@ -26,12 +29,13 @@ peakheight2= 0.5
 
 #radius (in terms of Swarzchild radii) on a log scale
 logr= np.linspace(0.5, 4, 100000) #already log
-print(logr)
-#print(help(laplace.ppf))
-#plt.plot(logr, prob(logslope, intercept, width1, width2, peakheight1, peakheight2, center1, center2, logr))
+#print(logr)
 
-test1=laplace.pdf(logr,2,width1/100)
-plt.plot(logr, )
+plt.plot(logr, prob(logslope, intercept, width1, width2, peakheight1, peakheight2, center1, center2, logr))
+
+#print(help(laplace.ppf))
+#test1=laplace.pdf(logr,2,width1/100)
+#plt.plot(logr, test1)
 plt.xlabel(r'log($\frac{r}{R_s}$)')
 plt.ylabel('prob(BBH location)')
 plt.show()
