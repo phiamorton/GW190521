508dc7bf61a177856ef230abd2a855a83a2a290f
phiamorton <mortonso@oregonstate.edu>
2023-07-05 12:10:07 +0200

    made prior parameters a default in the function

--- radius_prior.py
+++ radius_prior.py
@@ -4,7 +4,16 @@ from scipy.stats import laplace
 
 
 
-def rad_prior(logslope, intercept, width1, width2, peakheight1, peakheight2, center1, center2, logr):
+def rad_prior(logr):
+    #linear in log scale:
+    logslope= 1
+    intercept=1
+
+    #Laplacian/resonance peaks:
+    center1=0.85
+    center2=1.61
+    width1=np.log(2)
+    width2=np.log(2) #2 R_s on log scale
     linear= logslope * logr +intercept
     #normalize it in log space??????
     #area= logslope*logr.max()**2+intercept*logr.max() - (logslope*logr.min()**2+intercept*logr.min())
@@ -17,23 +26,12 @@ def rad_prior(logslope, intercept, width1, width2, peakheight1, peakheight2, cen
     peak2=(laplace.pdf(logr, center2, width2/100))/10
     return linear + peak1 +peak2      # /3 if each is normalized IF WANT TO NORMALIZE? MAYBE?
 
-#linear in log scale:
-logslope= 1
-intercept=1
-
-#Laplacian/resonance peaks:
-center1=0.85
-center2=1.61
-width1=np.log(2)
-width2=np.log(2) #2 R_s on log scale
-peakheight1= 0.5
-peakheight2= 0.5
 
 #radius (in terms of Swarzchild radii) on a log scale
 logr= np.linspace(0.5, 4, 100000) #already log
 #print(logr)
 
-plt.plot(logr, rad_prior(logslope, intercept, width1, width2, peakheight1, peakheight2, center1, center2, logr))
+plt.plot(logr, rad_prior(logr))
 
 #print(help(laplace.ppf))
 #test1=laplace.pdf(logr,2,width1/100)
