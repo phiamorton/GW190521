53a364c3d33a738b16fdd00b5a84cc7243924f2d
phiamorton <mortonso@oregonstate.edu>
2023-07-18 12:23:39 +0200

    oops

--- overplot_posteriors.py
+++ overplot_posteriors.py
@@ -12,10 +12,11 @@ from figaro.cosmology import CosmologicalParameters
 
 #LVK distribution:
 samples_in, name = load_single_event('GW190521.h5',par= ['mc_detect','luminosity_distance']) 
+#detector frame M_C?
 
 
 #redshifted model from conditioned draws:
-dpgmm_file = 'conditioned_density_draws.pkl' #non-redshifted M_c
+dpgmm_file = 'conditioned_density_draws.pkl' #non-redshifted M_c ?? OR IS IT
 #the conditional distribution (based on EM sky location)
 #z_c from EM counterpart candidate https://arxiv.org/pdf/2006.14122.pdf at ~2500 Mpc
 z_c = 0.438
@@ -34,15 +35,15 @@ else:
 
 
 samples_out = np.column_stack([post[lab] for lab in mymodel.names])
-samples_out[:,0] = np.exp(samples_out[:,0])
 
 omega = CosmologicalParameters(0.674, 0.315, 0.685, -1., 0.)
 DL_em = omega.LuminosityDistance_double(z_c)
+print(DL_em)
 reconstruction= samples_out[:,[0,1]]
     #reconstruction[:,0]=np.exp(reconstruction[:,0]) #use if samples of r are log
-r=samples_out[:,0]
+r=np.exp(samples_out[:,0])
 vel=1./np.sqrt(2*(r-1))  #the magnitude at a given distance from SMBH
-vel_LoS = vel * np.cos(samples_out[:,2]) #* np.cos(samples[:,3]) * np.cos(samples[:,4]) #Ive created a monster :((
+vel_LoS = vel * samples_out[:,2] #* np.cos(samples[:,3]) * np.cos(samples[:,4]) #Ive created a monster :((
     #gamma=lorentz factor
 gamma = 1./np.sqrt(1 - vel**2)
 
@@ -51,15 +52,14 @@ gamma = 1./np.sqrt(1 - vel**2)
 z_rel = gamma * (1 + vel_LoS) - 1
 
     #z_grav (r)
-z_grav = 1./np.sqrt(1 -(reconstruction[:,0])**-1 ) - 1 
+z_grav = 1./np.sqrt(1 - 1./r) - 1 
     #D_L eff (z_c, z_rel, z_grav, D_L)
 D_eff = (1+z_rel)**2 * (1+z_grav) * DL_em 
 
-samples_out[:,0]=D_eff
+M_eff = (1+z_c) * (1 + z_rel) * (1 + z_grav) * samples_out[:,1]
 
-M_eff = (1+z_c) * (1 + z_rel) * (1 + z_grav) * reconstruction[:,1]
-
-samples_out[:,1]=M_eff 
+# samples_out[:,1]=M_eff 
+samples_out = np.array([M_eff, D_eff]).T
 
 #add in conditioned dist. 
 from figaro.plot import plot_multidim 
@@ -86,8 +86,8 @@ conditioned_draws = condition(draws,[ra_EM_rad,dec_EM_rad], [2,3], norm=True, fi
 
 # fig=plot_multidim(conditioned_draws, samples = samples_out[:,[1,0]],labels = [ 'M_c effective ',' D_L effective']) 
 fig=plot_multidim(conditioned_draws, labels = ['M_c', 'D_{effective}'], units=['M_\\odot', 'Mpc'])
-redshifted=corner(samples_out[:,[1,0]], color='green',fig=fig, label='redshift model')
-LVK=corner(samples_in[:,[0,1]], color='grey', fig=fig, label='LVK')
+fig=corner(samples_out, color='green',fig=fig, label='redshift model', hist_kwargs ={'density':True})
+fig=corner(samples_in[:,[0,1]], color='grey', fig=fig, label='LVK', hist_kwargs ={'density':True})
 
 #agh making legends is stupid!!
 import matplotlib.lines as mlines
