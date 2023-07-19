f5049961e4eef398a937932540138c1c7fd4af44
phiamorton <mortonso@oregonstate.edu>
2023-07-19 11:03:56 +0200

    models for no association with EM

---
+++ no_EM_model.py
@@ -0,0 +1,73 @@
+import raynest
+import raynest.model
+import numpy as np
+import matplotlib.pyplot as plt 
+from corner import corner
+import h5py
+
+from scipy.special import logsumexp
+from numba import njit
+
+from figaro.cosmology import CosmologicalParameters
+from figaro.load import load_density
+from figaro.likelihood import logsumexp_jit
+from figaro.plot import plot_multidim 
+
+from priors import rad_prior, pl_peak_LVK, pl_peak_no_tapering 
+
+
+class noEM_model_plpk(raynest.model.Model):
+
+    def __init__(self, draws):
+        super(noEM_model_plpk,self).__init__()
+        self.draws=draws
+        self.N_draws = len(self.draws)
+        self.ones    = np.ones(self.N_draws)
+    
+        
+        
+        self.names= ['M_C',# M_C true chirp mass
+                     'z_c'] #D_L luminosity distance
+                
+        self.bounds =[ [0.,300.], [0,2] ]
+        
+    def log_prior(self,x):
+
+        logp=super(noEM_model_plpk,self).log_prior(x)
+
+        if np.isfinite(logp):    
+            logp_M_c = pl_peak_LVK(x['M_C']) #power law +peak ??
+            return logp_M_c
+        else:
+            return -np.inf
+
+    def log_likelihood(self,x):
+        DL = CosmologicalParameters(0.674,0.315,0.685,-1.,0.).LuminosityDistance_double(x['z_c'])
+        
+        M_eff = (1+x['z_c'])* x['M_C'] #chirp mass with cosmological redshift= M_eff
+
+        pt = np.atleast_2d([M_eff, DL]) #DL not conditioned on EM candidate
+        
+        logl = self.draws[0]._fast_logpdf(pt)  #one draw
+        return logl
+
+
+dpgmm_file = 'draws_GW190521_mc_dist.pkl' #detector frame M_C, not conditioned
+
+GW_posteriors = load_density(dpgmm_file)
+
+noEM_plpk_model= noEM_model_plpk(GW_posteriors)
+
+postprocess=True
+if not postprocess:
+    nest_noEM_plpk = raynest.raynest(noEM_plpk_model, verbose=2, nnest=1, nensemble=1, nlive=2000, maxmcmc=5000, output = 'inference_noEM_plpk/')
+    nest_noEM_plpk.run(corner = True)
+    post_noEM_plpk = nest_noEM_plpk.posterior_samples.ravel()
+else:
+    with h5py.File('inference_noEM_plpk/raynest.h5', 'r') as f:
+        no_EM_plpk_logZ= np.array(f['combined']['logZ'])
+        post_noEM_plpk = np.array(f['combined']['posterior_samples'])
+
+samples1 = np.column_stack([post_noEM_plpk[lab1] for lab1 in noEM_plpk_model.names])
+fig = corner(samples1, labels = ['$M_c$', 'z_c'], truths = [63.3, None])
+fig.savefig('noEM_plpk_posterior.pdf', bbox_inches = 'tight')
