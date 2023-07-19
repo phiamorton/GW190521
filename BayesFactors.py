418db990d9677851ac14117faa06c442f4547d00
phiamorton <mortonso@oregonstate.edu>
2023-07-19 11:36:11 +0200

    - NOT /

--- BayesFactors.py
+++ BayesFactors.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import h5py
-
+ 
 #no association, pl+pk prior
 with h5py.File('inference_noEM_plpk/raynest.h5', 'r') as f:
     no_EM_plpk_logZ=  np.array(f['combined']['logZ'])
@@ -14,7 +14,7 @@ with h5py.File('inference/raynest.h5', 'r') as f:
 
 print("estimated logZ for no EM counterpart and pl+pk model = {0} ".format(no_EM_plpk_logZ))
 print("estimated logZ for redshift model with r prior= {0} ".format(redshift_logZ))
-print("Log Bayes' Factor redshift model vs nonredshifted= ", redshift_logZ - no_EM_plpk_logZ) 
+print("Log Bayes' Factor redshift model vs nonredshifted= ", redshift_logZ - no_EM_plpk_logZ) #- not / for log"
 
 comparerprior=True
 if comparerprior==True:
