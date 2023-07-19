e80b2c58ac07292596cbd0bd1156a9632325a6a4
phiamorton <mortonso@oregonstate.edu>
2023-07-19 11:35:17 +0200

    logZ prior

--- BayesFactors.py
+++ BayesFactors.py
@@ -12,15 +12,15 @@ with h5py.File('inference/raynest.h5', 'r') as f:
     redshift_logZ=  np.array(f['combined']['logZ'])
 
 
-print("estimated logZ for no redshift = {0} ".format(no_EM_plpk_logZ))
-print("estimated logZ for redshift = {0} ".format(redshift_logZ))
-print("Log Bayes' Factor redshift model vs nonredshifted= ", redshift_logZ/no_EM_plpk_logZ) 
+print("estimated logZ for no EM counterpart and pl+pk model = {0} ".format(no_EM_plpk_logZ))
+print("estimated logZ for redshift model with r prior= {0} ".format(redshift_logZ))
+print("Log Bayes' Factor redshift model vs nonredshifted= ", redshift_logZ - no_EM_plpk_logZ) 
 
 comparerprior=True
 if comparerprior==True:
     #redshift model with uniform prior on r
     with h5py.File('inference_norprior/raynest.h5', 'r') as f:
         nor_logZ = np.array(f['combined']['logZ'])
-    print("estimated logZ for no redshift = {0} ".format(nor_logZ))
-    print("estimated logZ for redshift = {0} ".format(redshift_logZ))
-    print("Log Bayes' Factor r prior vs no r prior redshift model", redshift_logZ/nor_logZ) 
+    print("estimated logZ for no prior on r (redshift model)= {0} ".format(nor_logZ))
+    print("estimated logZ for prior on r (redshift model) = {0} ".format(redshift_logZ))
+    print("Log Bayes' Factor r prior vs no r prior redshift model", redshift_logZ-nor_logZ) 
