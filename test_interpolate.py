19df9357f753bc7d99afad99c5ef9a284f20588f
phiamorton <mortonso@oregonstate.edu>
2023-07-20 17:01:24 +0200

    conditioned too

--- test_interpolate.py
+++ test_interpolate.py
@@ -20,36 +20,61 @@ from figaro.load import load_density, save_density
 #     diff =(interp([xi,yi])- gauss([xi,yi]) )
 #     print(diff)
 
-dpgmm_file = 'primarymass/marginalized_density_draws_M1_and_DL.pkl' #detector frame M_1 and DL, marginalized over sky position
+marginal_density=True
+if marginal_density:
+    dpgmm_file = 'primarymass/marginalized_density_draws_M1_and_DL.pkl' #detector frame M_1 and DL, marginalized over sky position
 
+    GW_posteriors = load_density(dpgmm_file)
 
-GW_posteriors = load_density(dpgmm_file)
+    draws = load_density(dpgmm_file)
 
-draws = load_density(dpgmm_file)
+    #bounds over which to interpolate for the parameters
+    M_1=np.linspace(0,300,202)[1:-1]
+    D_L=np.linspace(0,10000,202)[1:-1]
+    #print(M_1.shape, D_L.shape)
+    MM, DD = np.meshgrid(M_1, D_L)
 
-#bounds over which to interpolate for the parameters
-M_1=np.linspace(0,300,102)[1:-1]
-D_L=np.linspace(0,10000,202)[1:-1]
-#print(M_1.shape, D_L.shape)
-MM, DD = np.meshgrid(M_1, D_L)
+    draws_pdf = np.log(np.mean([d.pdf(np.array([MM.flatten(), DD.flatten()]).T) for d in draws], axis = 0).reshape(len(D_L), len(M_1)) )
 
-draws_pdf = np.log(np.mean([d.pdf(np.array([MM.flatten(), DD.flatten()]).T) for d in draws], axis = 0).reshape(len(D_L), len(M_1)) )
+    interp_figaro= interp2d(M_1, D_L, draws_pdf, bounds_error=False)
 
-interp_figaro= interp2d(M_1, D_L, draws_pdf, bounds_error=False)
+    # print(interp_figaro([100,5000]))
+    # print(np.mean([d.pdf([100,5000]) for d in draws], axis = 0))
+    # print(interp_figaro([80,4000]))
+    # print(np.mean([d.pdf([80,4000]) for d in draws], axis = 0))
 
-# print(interp_figaro([100,5000]))
-# print(np.mean([d.pdf([100,5000]) for d in draws], axis = 0))
-# print(interp_figaro([80,4000]))
-# print(np.mean([d.pdf([80,4000]) for d in draws], axis = 0))
+    plt.contourf( DD, MM, interp_figaro(M_1, D_L))
+    # corner(M_1, D_L, draws[0].pdf(np.array([MM.flatten(), DD.flatten()]).T).reshape(len(M_1), len(D_L)) )
+    plt.savefig("checking_interpolant.pdf")
 
-plt.contourf( DD, MM, interp_figaro(M_1, D_L))
-# corner(M_1, D_L, draws[0].pdf(np.array([MM.flatten(), DD.flatten()]).T).reshape(len(M_1), len(D_L)) )
-plt.savefig("checking_interpolant.pdf")
+    import pickle
+    filename='Marginalized_interpolation.pkl'
+    with open(filename, 'wb') as f:
+        pickle.dump(interp_figaro, f)
 
-import pickle
-filename='Marginalized_interpolation.pkl'
-with open(filename, 'wb') as f:
-    pickle.dump(interp_figaro, f)
+conditional_density=True
+if conditional_density:
+    dpgmm_file = 'primarymass/conditioned_density_draws_M1_and_DL.pkl' #detector frame M_1 and DL, marginalized over sky position
 
-#with open(dpgmm_file, 'rb') as f:
-#     GW_posteriors= pickle.load(f)
+    GW_posteriors = load_density(dpgmm_file)
+
+    draws = load_density(dpgmm_file)
+
+    #bounds over which to interpolate for the parameters
+    M_1=np.linspace(0,300,202)[1:-1]
+    D_L=np.linspace(0,10000,202)[1:-1]
+    #print(M_1.shape, D_L.shape)
+    MM, DD = np.meshgrid(M_1, D_L)
+
+    draws_pdf = np.log(np.mean([d.pdf(np.array([MM.flatten(), DD.flatten()]).T) for d in draws], axis = 0).reshape(len(D_L), len(M_1)) )
+
+    interp_figaro= interp2d(M_1, D_L, draws_pdf, bounds_error=False)
+
+    # plt.contourf( DD, MM, interp_figaro(M_1, D_L))
+    # # corner(M_1, D_L, draws[0].pdf(np.array([MM.flatten(), DD.flatten()]).T).reshape(len(M_1), len(D_L)) )
+    # plt.savefig("checking_interpolant.pdf")
+
+    import pickle
+    filename='conditional_interpolation.pkl'
+    with open(filename, 'wb') as f:
+        pickle.dump(interp_figaro, f)
