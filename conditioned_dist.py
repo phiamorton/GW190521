f0567d30fa4d25ee4a50f5dcc061edf06a97149d
phiamorton <mortonso@oregonstate.edu>
2023-07-24 17:54:06 +0200

    not normalized

--- conditioned_dist.py
+++ conditioned_dist.py
@@ -7,8 +7,7 @@ from figaro.marginal import condition
 """start with pkl file of r draws from figaro 
 optputs a file with conditioned draws and a plot of the conditioned distribution"""
 
-
-filepath= 'draws_GW190521.pkl'
+filepath= 'primarymass/draws_GW190521.pkl' #MUST be the one with primary mass (in primary mass directory)
 
 #the conditioned dist. 
 ra_EM, dec_EM = 192.42625 , 34.8247
@@ -18,13 +17,24 @@ dec_EM_rad=dec_EM/180*np.pi
 
 draws = load_density(filepath)
 
-conditioned_draws = condition(draws,[ra_EM_rad,dec_EM_rad], [2,3], norm=True, filter=True, tol=1e-3)
-plot_multidim(conditioned_draws, name= 'conditioned_distribution_M1', labels=['M_1', 'D_L'], units=['M_\\odot', 'Mpc'])
+conditioned_draws = condition(draws,[ra_EM_rad,dec_EM_rad], [2,3], norm=True, filter=False, tol=1e-3)
+
+#plot_multidim(conditioned_draws, name= 'conditioned_distribution_M1', labels=['M_1', 'D_L'], units=['M_\\odot', 'Mpc'])
 
 save_density(conditioned_draws, name='conditioned_density_draws_M1_and_DL')
 
-print([d.n_cl for d in conditioned_draws])
+conditioned_draws_nF = condition(draws,[ra_EM_rad,dec_EM_rad], [2,3], norm=False, filter=False, tol=1e-3)
+#print(conditioned_draws_nF)
+save_density(conditioned_draws_nF, name='conditioned_density_draws_M1_and_DL_nF')
+
+#dont want to use normalized draws because it fucks up the Bayes factors
+#print([d.n_cl for d in conditioned_draws_nF])
 
 #now the marginal one too from the no association model
-draws_m1_marginal = [d.marginalise([2,3]) for d in draws] 
-save_density(draws_m1_marginal, name='marginalized_density_draws_M1_and_DL')
+make_marginal=False
+if make_marginal:
+    filepath= 'draws_allsky_GW190521/draws_GW190521.pkl' 
+    draws = load_density(filepath)
+    draws_m1_marginal = [d.marginalise([2,3]) for d in draws] 
+    save_density(draws_m1_marginal, name='marginalized_density_draws_M1_and_DL')
+
