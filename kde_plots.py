6912ebef158fb6ffa3e05a78ad335cd2476374a6
phiamorton <mortonso@oregonstate.edu>
2023-07-27 19:31:10 +0200

    pretty colors

--- kde_plots.py
+++ kde_plots.py
@@ -13,13 +13,13 @@ kernel=gaussian_kde(data)
 fig, ax = plt.subplots()
 #H_0 from H_0 paper from Planck and ShoES measurement med +-1 sigma
 
-ax.axvline( 67.4, lw=1, label='Planck', color='blue') #67.4 +-0.5 from 2018 https://arxiv.org/abs/1807.06209 
-ax.axvline( 67.4, lw=2, alpha=0.4, color='blue') #67.4 +-0.5 from 2018 https://arxiv.org/abs/1807.06209 
-ax.axvspan( 72.04, 74.06, label='SH0ES', color='skyblue') #https://arxiv.org/abs/2112.04510 73.04+-1.04
-ax.axvspan( 71.04, 75.1,  alpha=0.4, color='skyblue') #https://arxiv.org/pdf/2112.04510.pdf 
+ax.axvline( 67.4, lw=1,label='Planck' , color='mediumaquamarine') #67.4 +-0.5 from 2018 https://arxiv.org/abs/1807.06209 
+ax.axvline( 67.4, lw=2, alpha=0.4, color='mediumaquamarine') #67.4 +-0.5 from 2018 https://arxiv.org/abs/1807.06209 
+ax.axvspan( 72.04, 74.06, label='SH0ES', color='wheat') #https://arxiv.org/abs/2112.04510 73.04+-1.04
+ax.axvspan( 71.04, 75.1,  alpha=0.4, color='wheat') #https://arxiv.org/pdf/2112.04510.pdf 
 
 H0=np.linspace(20,200, 10**3)
-ax.plot(H0, kernel(H0), linewidth=1, label='GW170817', color='orchid')
+ax.plot(H0, kernel(H0), linewidth=1, label='GW170817', color='royalblue')
 
 #with prior
 with h5py.File('inference_H_GW17/raynest.h5', 'r') as f:
@@ -27,7 +27,7 @@ with h5py.File('inference_H_GW17/raynest.h5', 'r') as f:
     samples_GW17 = np.column_stack(post['H_0'])
 #print(samples_GW17)
 kernel=gaussian_kde(samples_GW17)
-ax.plot(H0, kernel(H0), linewidth=1, label='GW190521 + GW170817', color='purple')
+ax.plot(H0, kernel(H0), linewidth=1, label='GW190521 + GW170817', color='darkgreen')
 
 
 
@@ -37,7 +37,7 @@ with h5py.File('inference_H/raynest.h5', 'r') as f:
     samples_H= np.column_stack(post['H_0'])
 #print(samples_H)
 kernel=gaussian_kde(samples_H)
-ax.plot(H0, kernel(H0), linewidth=1, label='GW190521', color='magenta')
+ax.plot(H0, kernel(H0), linewidth=1, label='GW190521', color='maroon')
 
 
 
