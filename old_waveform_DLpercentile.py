a247dff97db4ff41ec943bec50e779989f3882b8
phiamorton <mortonso@oregonstate.edu>
2023-10-05 10:54:50 -0700

    initial commit

---
+++ old_waveform_DLpercentile.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+import h5py
+from figaro.load import load_density
+
+D_L=np.linspace(1,10000,202)[1:-1]
+
+#LVK old waveform 
+filepath='old_waveform/GW190521_posterior_samples.h5'
+
+draws= load_density(filepath)
+draws_pdf = np.mean([d.pdf(D_L.T) for d in draws], axis = 0).reshape(len(D_L)) 
+perc = np.sum(draws_pdf[D_L<DL_em]*(D_L[1]-D_L[0]))
+print(perc*100)
+
