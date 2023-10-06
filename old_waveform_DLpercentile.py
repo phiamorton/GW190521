25eb0cd2aae7c4269bfc60b654c665a01a23488b
Phia Morton <mortonso@oregonstate.edu>
2023-10-05 18:06:46 -0700

    add Figaro cosmology

--- old_waveform_DLpercentile.py
+++ old_waveform_DLpercentile.py
@@ -2,6 +2,10 @@
 import numpy as np
 import h5py
 from figaro.load import load_density
+from figaro.cosmology import CosmologicalParameters
+
+z_c=0.438
+DL_em = CosmologicalParameters(0.674,0.315,0.685,-1.,0.).LuminosityDistance_double(z_c)
 
 D_L=np.linspace(1,10000,202)[1:-1]
 
