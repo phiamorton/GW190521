2368f5428bcc08feb25cb9f227371ce4d356cb2e
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2023-10-14 18:09:21 +0200

    fixedÃ

--- old_waveform_DLpercentile.py
+++ old_waveform_DLpercentile.py
@@ -1,19 +1,17 @@
 
 import numpy as np
 import h5py
-from figaro.load import load_density
+from figaro.load import load_single_event
 from figaro.cosmology import CosmologicalParameters
 
 z_c=0.438
 DL_em = CosmologicalParameters(0.674,0.315,0.685,-1.,0.).LuminosityDistance_double(z_c)
 
-D_L=np.linspace(1,10000,202)[1:-1]
-
 #LVK old waveform 
 filepath='old_waveform/GW190521_posterior_samples.h5'
 
-draws= load_density(filepath)
-draws_pdf = np.mean([d.pdf(D_L.T) for d in draws], axis = 0).reshape(len(D_L)) 
-perc = np.sum(draws_pdf[D_L<DL_em]*(D_L[1]-D_L[0]))
+samples, name = load_single_event(filepath, par = ['luminosity_distance'], waveform = 'imr')
+samples = np.sort(samples.flatten())
+perc = np.argmin(abs(samples - DL_em))/len(samples)
 print(perc*100)
 
